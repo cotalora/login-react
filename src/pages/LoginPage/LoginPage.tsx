5521f3203bd8fb9c8b71c630abb7929014e3beb9
cotalora <67478427+cotalora@users.noreply.github.com>
2022-09-15 17:15:35 -0500

    Segunda parte de login

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Avatar, Button, Divider, TextField, InputLabel, Checkbox, FormControlLabel } from '@mui/material';
 import Carousel from 'react-material-ui-carousel'
 import './LoginPage.scss';
 
@@ -35,7 +35,7 @@ export const LoginPage = () => {
                             <Carousel
                                 autoPlay={true}
                                 navButtonsAlwaysInvisible={true}
-                                interval={3000}
+                                interval={5000}
                                 indicatorContainerProps={{
                                     className: 'carousel-indicator-container'
                                 }}
@@ -47,16 +47,160 @@ export const LoginPage = () => {
                                 }}
                             >
                                 <Box className="info-item">
-                                    ITEM 1
+                                    <Typography
+                                        paragraph={true}
+                                        className="info-item-text"
+                                    >
+                                        ¡Simplemente increible! Estoy realmete sarisfecho
+                                        con mi proyecto y mi negocio. Esto es absolutamente
+                                        fantastico
+                                    </Typography>
+                                    <Box className="info-item-person-container">
+                                        <Avatar
+                                            alt="Remy Sharp"
+                                            src="src/assets/img/1.jpg"
+                                            variant="rounded"
+                                        />
+                                        <Box className="info-item-person-text-container">
+                                            <Typography
+                                                variant='body2'
+                                                className='info-item-person-name'
+                                            >
+                                                Timson K.
+                                            </Typography>
+                                            <Typography
+                                                variant='body2'
+                                                className='info-item-person-profession'
+                                            >
+                                                Developer Freelanzer
+                                            </Typography>
+                                        </Box>
+                                    </Box>
                                 </Box>
                                 <Box className="info-item">
-                                    ITEM 2
+                                    <Typography
+                                        paragraph={true}
+                                        className="info-item-text"
+                                    >
+                                        ¡Simplemente increible! Estoy realmete sarisfecho
+                                        con mi proyecto y mi negocio. Esto es absolutamente
+                                        fantastico
+                                    </Typography>
+                                    <Box className="info-item-person-container">
+                                        <Avatar
+                                            alt="Remy Sharp"
+                                            src="src/assets/img/2.jpg"
+                                            variant="rounded"
+                                        />
+                                        <Box className="info-item-person-text-container">
+                                            <Typography
+                                                variant='body2'
+                                                className='info-item-person-name'
+                                            >
+                                                Timson K.
+                                            </Typography>
+                                            <Typography
+                                                variant='body2'
+                                                className='info-item-person-profession'
+                                            >
+                                                Developer Freelanzer
+                                            </Typography>
+                                        </Box>
+                                    </Box>
                                 </Box>
                             </Carousel>
                         </Box>
                     </Box>
                     <Box className="form-container">
-                        <h1>FORMULARIO</h1>
+                        <Typography
+                            variant='h2'
+                            className="form-title"
+                        >
+                            Inicio de Sesión
+                        </Typography>
+                        <Typography
+                            className="form-text"
+                            variant='body2'
+                        >
+                            Ingresa tus credecciales para acceder a tu cuenta
+                        </Typography>
+                        <form>
+                            <Box className="form-buttons-social">
+                                <Button
+                                    startIcon={
+                                        <img
+                                            className='form-button-icon'
+                                            src='src/assets/img/google-icon.svg'
+                                        />
+                                    }
+                                    className="form-button-social"
+                                >
+                                    Inicia sesión con Google
+                                </Button>
+                                <Button
+                                    startIcon={
+                                        <img
+                                            className='form-button-icon'
+                                            src='src/assets/img/facebook-icon.svg'
+                                        />
+                                    }
+                                    className="form-button-social"
+                                >
+                                    Inicia sesión con Facebook
+                                </Button>
+                            </Box>
+                            <Divider className='form-divider'>o</Divider>
+                            <Box className="form-control">
+                                <InputLabel
+                                    htmlFor="email"
+                                    className="form-control-label"
+                                >
+                                    Correo electrónico
+                                </InputLabel>
+                                <TextField
+                                    id="email"
+                                    variant="outlined"
+                                    type="email"
+                                    className="form-control-input"
+                                />
+                            </Box>
+                            <Box className="form-control">
+                                <InputLabel
+                                    htmlFor="password"
+                                    className="form-control-label"
+                                >
+                                    Contraseña
+                                </InputLabel>
+                                <TextField
+                                    id="password"
+                                    variant="outlined"
+                                    type="password"
+                                    className="form-control-input"
+                                />
+                            </Box>
+                            <Box className="form-control">
+                                <FormControlLabel
+                                    className='form-control-checkbox'
+                                    control={
+                                        <Checkbox
+                                            disableRipple={true}
+                                            className='checkbox'
+                                            sx={{
+                                                '&.Mui-checked': {
+                                                    color: '#3C37FF',
+                                                },
+                                            }}
+                                        />}
+                                    label="Recordar información"
+                                />
+                            </Box>
+                            <Button
+                                type='submit'
+                                className="button-principal"
+                            >
+                                Iniciar sesión
+                            </Button>
+                        </form>
                     </Box>
                 </Box>
             </Box>
