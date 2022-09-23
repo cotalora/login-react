f0d1178a668c6de1c7493e3f8bb90e3b5cb633a8
cotalora <67478427+cotalora@users.noreply.github.com>
2022-09-23 16:12:48 -0500

    Refactorización de código

--- src/router/MainRoute/MainRoute.tsx
+++ src/router/MainRoute/MainRoute.tsx
@@ -1,18 +1,23 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
+import { Grow } from "@mui/material";
 import { AnotherPage } from '../../pages/AnotherPage/AnotherPage'
 
 export const MainRoute = () => {
     return (
         <>
-            <div>NAVBAR</div>
+            <Grow in={true}>
+                <div>
+                    <div>NAVBAR</div>
 
-            <div className="container">
-                <Routes>
-                    <Route path="other" element={<AnotherPage />} />
+                    <div className="container">
+                        <Routes>
+                            <Route path="other" element={<AnotherPage />} />
 
-                    <Route path="/*" element={<Navigate to="/other" />} />
-                </Routes>
-            </div>
+                            <Route path="/*" element={<Navigate to="/other" />} />
+                        </Routes>
+                    </div>
+                </div>
+            </Grow>
         </>
     )
 }
